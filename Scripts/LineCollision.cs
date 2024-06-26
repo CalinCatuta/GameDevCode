fa127397f5b5367a3ae3346305ac6880a4f5aac8
Calin Catuta <77282302+CalinCatuta@users.noreply.github.com>
2024-06-26 17:12:50 +0300

    Update LineCollision.cs

--- Scripts/LineCollision.cs
+++ Scripts/LineCollision.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -35,37 +35,47 @@ public class LineCollision : MonoBehaviour
     }
 
     private List<Vector2> CalculateColliderPoints() {
-        //Get All positions on the line renderer
+        // Get All positions on the line renderer
         Vector3[] positions = lc.GetPositions();
 
-        //Get the Width of the Line
+        // Get the Width of the Line
         float width = lc.GetWidth();
 
-        //m = (y2 - y1) / (x2 - x1)
+        // Calculate the slope (m)
         float m = (positions[1].y - positions[0].y) / (positions[1].x - positions[0].x);
-        float deltaX = (width / 2f) * (m / Mathf.Pow(m * m + 1, 0.5f));
-        float deltaY = (width / 2f) * (1 / Mathf.Pow(1 + m * m, 0.5f));
 
-        //Calculate the Offset from each point to the collision vertex
-        Vector3[] offsets = new Vector3[2];
-        offsets[0] = new Vector3(-deltaX, deltaY);
-        offsets[1] = new Vector3(deltaX, -deltaY);
+        // Clamping values to prevent excessive spreading
+        float maxDelta = width / 4f;
 
-        //Generate the Colliders Vertices
+        float deltaX = Mathf.Clamp((width / 2f) * (m / Mathf.Pow(m * m + 1, 0.5f)), -maxDelta, maxDelta);
+        float deltaYUpper = Mathf.Clamp((width / 4f) * (1 / Mathf.Pow(1 + m * m, 0.5f)), -maxDelta, maxDelta); // Reduced deltaY for upper points
+        float deltaYLower = Mathf.Clamp((width / 8f) * (1 / Mathf.Pow(1 + m * m, 0.5f)), -maxDelta, maxDelta); // Further reduced deltaY for lower points
+
+        // Calculate the Offset from each point to the collision vertex
+        Vector3[] offsetsUpper = new Vector3[2];
+        Vector3[] offsetsLower = new Vector3[2];
+        offsetsUpper[0] = new Vector3(-deltaX, deltaYUpper);
+        offsetsUpper[1] = new Vector3(deltaX, -deltaYUpper);
+        offsetsLower[0] = new Vector3(-deltaX, deltaYLower);
+        offsetsLower[1] = new Vector3(deltaX, -deltaYLower);
+
+        // Generate the Colliders Vertices
         List<Vector2> colliderPositions = new List<Vector2> {
-            positions[0] + offsets[0],
-            positions[1] + offsets[0],
-            positions[1] + offsets[1],
-            positions[0] + offsets[1]
-        };
+        positions[0] + offsetsUpper[0],
+        positions[1] + offsetsUpper[0],
+        positions[1] + offsetsLower[1],
+        positions[0] + offsetsLower[1]
+    };
 
         return colliderPositions;
     }
 
+
+
+
     private void OnTriggerEnter2D(Collider2D col) {
         if (col.gameObject.TryGetComponent(out IDamageable target))
         {
-            Debug.Log("Enemy Hit");
             target.TakeDamage(currentDamage);
         }
     }
